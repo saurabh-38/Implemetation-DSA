2479eca3c1eb54de642695a806cf4e86208350cc
Saurabh gupta <64426037+saurabh-38@users.noreply.github.com>
2022-04-08 18:28:34 +0530

    submit

---
+++ C Question/interesting_story.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define rep(i, a, b) for(int i = (a); i < (b); i++)
+#define vll vector<ll>
+#define vlll vector<vll>
+#define mp map<ll,ll>
+ll const tax=1e9+7;
+#define pb push_back
+void setIO()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+}
+#define rasengan ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+signed main(){
+  
+setIO();
+rasengan
+        ll t;
+        cin>>t;
+        while(t--){
+            
+        }
+
+
+}
+
